fdc9357f612afdf1fe6eb1d1926aa05ce23e7f78
Thien Vo <thien.vo@x-factory.jp>
2024-07-26 11:30:45 +0700

    install vue-tsc for agent & client

--- apps/client/nuxt.config.ts
+++ apps/client/nuxt.config.ts
@@ -3,5 +3,12 @@ import { defineNuxtConfig  } from "nuxt/config"
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@packages/ui'],
-  srcDir: "./src"
+  
+  srcDir: "./src",
+
+  typescript: {
+    shim: false,
+    strict: true,
+    typeCheck: false
+  }
 })
